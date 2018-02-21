9ffa3d9489c7a88e2c72bbfc8ee3d541142855cb
Sandeepkhandaynitk <khandaysandeep@gmail.com>
2018-02-21 14:21:21 +0530

    Boot strap Practice

---
+++ NT/Js/Rb2exx.js
@@ -0,0 +1,5 @@
+function popup(){
+	alert("Welcome");
+	confirm("are you above 18");
+	prompt("enter your name");
+}
---
+++ NT/Js/msg.js
@@ -0,0 +1,12 @@
+var age = prompt ("Enter your age ");
+if(age<=18){
+console.log("Your canntot Enter the vanue");
+}
+else if{
+	(age == 18 || age == 21);
+console.log("You can Enter but not Drink");
+}
+else
+{
+	console.log("your can enter the vanue and drink");
+}
